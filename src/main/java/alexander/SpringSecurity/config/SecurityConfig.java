1c5de86ffb181b350f65e1248b5e5be662e4401c
Kevin Alexander Sánchez Torres <69257228+alexanderBennington@users.noreply.github.com>
2025-07-02 06:41:30 +0000

    Sessiones

--- src/main/java/alexander/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/alexander/SpringSecurity/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -37,13 +39,19 @@ public class SecurityConfig {
                         .invalidSessionUrl("/login")
                         .maximumSessions(1)
                         .expiredUrl("/login")
+                        .sessionRegistry(sessionRegistry())
                 ) //Va a crear una sesion siempre y cuando no exista alguna otra
                 .build();
     }
 
+    @Bean
+    public SessionRegistry sessionRegistry(){
+        return new SessionRegistryImpl();
+    }
+
     public AuthenticationSuccessHandler successHandler(){
         return ((request, response, authentication) -> {
-            response.sendRedirect("/v1/index");
+            response.sendRedirect("/v1/session");
         });
     }
 }
